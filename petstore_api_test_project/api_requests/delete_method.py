adb5e6823d21a17649882e19c9c67fe979c3aa12
Zaur Mamedov <zaurmd1996@gmail.com>
2024-05-11 20:37:19 +0300

    Delete not using modules and files

--- petstore_api_test_project/api_requests/delete_method.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-def delete_pet(new_pet_id):
-    response = requests.delete(
-        url=f'https://petstore.swagger.io/v2/pet/{new_pet_id}'
-    )
-
-    return response
--- petstore_api_test_project/api_requests/get_method.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-def return_pet(new_pet_id):
-    response = requests.get(
-        url=f'https://petstore.swagger.io/v2/pet/{new_pet_id}'
-    )
-
-    return response
