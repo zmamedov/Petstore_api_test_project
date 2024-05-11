f9f465898e40eb7a0845ddf2df2ea0998cfeb2c3
Zaur Mamedov <zaurmd1996@gmail.com>
2024-05-11 21:47:20 +0300

    Fix file name in function validate_response_to_json_schema

--- petstore_api_test_project/utils/validate_schema.py
+++ petstore_api_test_project/utils/validate_schema.py
@@ -5,7 +5,7 @@ from petstore_api_test_project.utils.path import abs_path_from_project
 
 
 def validate_response_to_json_schema(json_schema, response):
-    json_file = abs_path_from_project(f'petstore_api_test_project\\json_schemas\\{json_schema}')
+    json_file = abs_path_from_project(f'petstore_api_test_project/json_schemas/{json_schema}')
 
     with open(json_file) as file:
         validate(response.json(), schema=json.loads(file.read()))
