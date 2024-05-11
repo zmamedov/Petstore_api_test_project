4d1d1bb56d0b25f19224e181b017b29c6e1aab6d
Zaur Mamedov <zaurmd1996@gmail.com>
2024-05-11 12:59:51 +0300

    Add test samples

---
+++ tests/test_petstore.py
@@ -0,0 +1,18 @@
+def test_add_new_pet_to_store():
+    pass
+
+
+def test_load_pet_image():
+    pass
+
+
+def test_change_pet_name():
+    pass
+
+
+def test_find_pet_by_id():
+    pass
+
+
+def test_delete_pet():
+    pass
