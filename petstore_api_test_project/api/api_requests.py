8428512f4c6a6e2b6d767a8916d5c8a9fd4cbd27
Zaur Mamedov <zaurmd1996@gmail.com>
2024-05-14 20:09:48 +0300

    Add attachments of request and response

--- petstore_api_test_project/api/api_requests.py
+++ petstore_api_test_project/api/api_requests.py
@@ -1,6 +1,10 @@
+import json
+
+import allure
 import requests
 import logging
 
+from allure_commons.types import AttachmentType
 from requests import Response
 
 
@@ -9,6 +13,7 @@ def get_request(url):
         url=url
     )
     response_logging(response)
+    response_attaching(response)
 
     return response
 
@@ -20,6 +25,7 @@ def post_request(url, headers, data):
         data=data
     )
     response_logging(response)
+    response_attaching(response)
 
     return response
 
@@ -29,6 +35,7 @@ def delete_request(url):
         url=url
     )
     response_logging(response)
+    response_attaching(response)
 
     return response
 
@@ -42,3 +49,25 @@ def response_logging(response: Response):
     logging.info("Request headers: " + str(response.request.headers))
     logging.info("Response code " + str(response.status_code))
     logging.info("Response: " + response.text)
+
+
+def response_attaching(response: Response):
+    allure.attach(
+        body=response.request.url,
+        name="Request url",
+        attachment_type=AttachmentType.TEXT,
+    )
+
+    if response.request.body:
+        allure.attach(
+            body=json.dumps(response.request.body, indent=4, ensure_ascii=True),
+            name="Request body",
+            attachment_type=AttachmentType.JSON,
+            extension="json",
+        )
+        allure.attach(
+            body=json.dumps(response.json(), indent=4, ensure_ascii=True),
+            name="Response",
+            attachment_type=AttachmentType.JSON,
+            extension="json",
+        )
